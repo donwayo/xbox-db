fa26625c7e201455f0236f093cc53c5aa42602e3
Eduardo Castellanos <eduardo.castellanos@siemens.com>
2017-03-27 17:53:45 +0200

    Add Game, Title, and Build to the admin page.

--- cxbx_compat/admin.py
+++ cxbx_compat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Build, Game, Title
+
+admin.site.register(Build)
+admin.site.register(Game)
+admin.site.register(Title)
