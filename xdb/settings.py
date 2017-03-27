b505284a94f785819d92389cbc2d6fc6e46a6276
Eduardo Castellanos <eduardo.castellanos@siemens.com>
2017-03-27 14:15:51 +0200

    Removed MongoDB

--- xdb/settings.py
+++ xdb/settings.py
@@ -91,14 +91,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'mongodb': {
-        'ENGINE': 'django_mongodb_engine',
-        'NAME': db_name,
-        'USER': username,
-        'PASSWORD': password,
-        'HOST': host,
-        'PORT': port
-    }
+    # 'mongodb': {
+    #    'ENGINE': 'django_mongodb_engine',
+    #    'NAME': db_name,
+    #    'USER': username,
+    #    'PASSWORD': password,
+    #    'HOST': host,
+    #    'PORT': port
+    #}
 }
 
 AUTH_PASSWORD_VALIDATORS = [
