f5c720a28748844349c11e1b0a92af8fbff3c4e2
Eduardo Castellanos <eduardo.castellanos@siemens.com>
2017-03-28 17:10:29 +0200

    Remove Google verification.

--- xdb/urls.py
+++ xdb/urls.py
@@ -17,12 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
 from .forms import LoginForm
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('cxbx_compat.urls')),
     url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
-    url(r'^logout/$', views.logout, {'next_page': '/login'}),
-    url(r'^google4c6b8f36d112339f\.html$', TemplateView.as_view(template_name='google4c6b8f36d112339f.html'))
+    url(r'^logout/$', views.logout, {'next_page': '/login'})
 ]
