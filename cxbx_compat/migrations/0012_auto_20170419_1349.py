fb2faf5b5a3a39803176dbed80ecd07ad0bd735b
Eduardo Castellanos <eduardo.castellanos@siemens.com>
2017-04-19 15:00:59 +0200

    Split the migrations in 3 for PostgreSQL

--- cxbx_compat/migrations/0012_auto_20170419_1349.py
+++ cxbx_compat/migrations/0012_auto_20170419_1349.py
@@ -3,24 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from xdb.utils.xbe import Xbe
 import uuid
-
-
-def signature_forwards_func(apps, schema_editor):
-    Executable = apps.get_model('cxbx_compat', 'Executable')
-    db_alias = schema_editor.connection.alias
-
-    all_xbe = Executable.objects.using(db_alias).all()
-
-    for xbe in all_xbe:
-        signature = bytes.fromhex(xbe.signature)
-        xbe.signature_hash = Xbe.decrypt_signature(signature).hex().upper()
-        xbe.save()
-
-
-def signature_reverse_func(apps, schema_editor):
-    pass
 
 
 class Migration(migrations.Migration):
@@ -41,11 +24,4 @@ class Migration(migrations.Migration):
             name='signature',
             field=models.CharField(max_length=512),
         ),
-        migrations.RunPython(signature_forwards_func, signature_reverse_func),
-        migrations.AlterField(
-            model_name='executable',
-            name='signature_hash',
-            field=models.CharField(max_length=40, unique=True, null=False),
-            preserve_default=False,
-        )
     ]
